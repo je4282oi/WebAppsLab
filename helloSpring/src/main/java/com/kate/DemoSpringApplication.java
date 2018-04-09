bc44755b602f29f1e41eb07a06d08b3e3c08ee82
je4282oi <35351785+je4282oi@users.noreply.github.com>
2018-04-09 09:53:28 -0500

    Add files via upload
    HelloSpring files

---
+++ helloSpring/src/main/java/com/kate/DemoSpringApplication.java
@@ -0,0 +1,22 @@
+package com.kate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@SpringBootApplication
+public class DemoSpringApplication {
+
+	@RequestMapping("/hello")
+	@ResponseBody
+	String helloPage() {
+		return "Hello from the hello page!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringApplication.class, args);
+	}
+}
