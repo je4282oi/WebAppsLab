76abd4453f832ab9a9b1d4082265da606730541c
K <nonsense@gmail.com>
2018-04-12 17:02:36 -0500

    Some testing Thursday 12th

--- helloSpring/src/main/java/com/kate/DemoSpringApplication.java
+++ helloSpring/src/main/java/com/kate/DemoSpringApplication.java
@@ -5,13 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
+//Labels needed for server to identify commands
 @Controller
 @SpringBootApplication
 public class DemoSpringApplication {
-	//Label needed for server to identify commands
 
+	//RequestMapping tells you url extension message with display at
+	//This displays to http://127.0.0.1:8080/hello
 	@RequestMapping("/hello")
+	//ResponseBody tells you message that will display
 	@ResponseBody
 	String helloPage() {
 		return "Hello from the hello page!";
--- src/main/java/com/kate/MetadataApplication.java
+++ src/main/java/com/kate/MetadataApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MetadataApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(MetadataApplication.class, args);
 	}
 }
