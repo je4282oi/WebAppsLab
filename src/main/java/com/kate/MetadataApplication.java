d3332634e0ae46453498b0321e2e24c3023303d3
K <nonsense@gmail.com>
2018-04-12 19:22:50 -0500

    One program here now

--- src/main/java/com/kate/MetadataApplication.java
+++ src/main/java/com/kate/MetadataApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MetadataApplication {
 
+	//This is the one that displays the plant photo and has colours etc.
+
 	public static void main(String[] args) {
 
 		SpringApplication.run(MetadataApplication.class, args);
